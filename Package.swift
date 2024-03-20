90d8693eb83ba8140335db17bfde220815516e12
Gabriel Comte <gabriel.comte@gmail.com>
2024-03-20 12:51:43 +0100

    Rename target

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
-// swift-tools-version:5.3
-
 import PackageDescription
 import Foundation
-
 let package = Package(
         name: "LipaLightningLib",
         platforms: [
@@ -19,8 +16,8 @@ let package = Package(
             url: "https://github.com/getlipa/lipa-lightning-lib-swift/releases/download/v0.41.6-beta/lipalightninglibFFI.xcframework.zip",
             checksum: "c982b5063781c7208058a52eb3facac82e680f6eaf0afb6795bbba46db1c2db6"),
             .target(
-                    name: "LipaLightningLib",
-                    dependencies: ["lipalightninglibFFI"]),
-                    path: "Source/LipaLightningLibMock"
+                    name: "LipaLightningLibMock",
+                    dependencies: ["lipalightninglibFFI"],
+                    path: "Source/LipaLightningLib"),
         ]
 )
