0bf64932d6d5103b95aadbd868a90bbf90a704d8
Max Lukashevich <lukashevich.maks@gmail.com>
2024-03-20 13:14:11 +0100

    Update target name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         products: [
             .library(
                 name: "LipaLightningLib",
-                targets: ["LipaLightningLib"]),
+                targets: ["LipaLightningLibMock"]),
         ],
         targets: [
             .binaryTarget(
